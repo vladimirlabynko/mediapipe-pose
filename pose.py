eabdeb30c0f12bf4ab87cc04f7d6919db845ef13
vladimir.l <vladimir.l@iomic.com>
2022-03-19 16:19:58 +0300

    [EDIT] input,output video

--- pose.py
+++ pose.py
@@ -4,8 +4,24 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 
-# For webcam input:
-cap = cv2.VideoCapture(0)
+input_video=("test.mp4")
+output_video=("output.mp4")
+
+cap = cv2.VideoCapture(input_video)
+
+if (cap.isOpened() == False):
+        print("Error opening the video file")
+    # Obtain frame size information using get() method
+image_width = int(cap.get(3))
+image_height = int(cap.get(4))
+frame_size = (image_width,image_height)
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+
+    # Initialize video writer object
+output = cv2.VideoWriter(output_video, cv2.VideoWriter_fourcc(*'mp4v'), fps, frame_size)
+
+
 with mp_pose.Pose(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as pose:
@@ -14,7 +30,7 @@ with mp_pose.Pose(
     if not success:
       print("Ignoring empty camera frame.")
       # If loading a video, use 'break' instead of 'continue'.
-      continue
+      break
 
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
@@ -31,7 +47,9 @@ with mp_pose.Pose(
         mp_pose.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
     # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+    output.write(image)
+
+
 cap.release()
+output.release()
+cv2.destroyAllWindows()
