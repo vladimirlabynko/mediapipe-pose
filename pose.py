21a1bc9e276c5d3d3ecc67f73c42c90fb5a3a6a8
vladimir.l <vladimir.l@iomic.com>
2022-03-20 13:11:30 +0300

    [ADD] choosing input and output file in terminal

--- pose.py
+++ pose.py
@@ -1,11 +1,5 @@
 import cv2
 import mediapipe as mp
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_holistic = mp.solutions.holistic
-
-import cv2
-import mediapipe as mp
 import argparse
 import sys
 
@@ -15,8 +9,8 @@ mp_holistic = mp.solutions.holistic
 
 
 
-def process_video(input,output):
-    cap = cv2.VideoCapture(input)
+def process_video(args):
+    cap = cv2.VideoCapture(args.input)
     if (cap.isOpened() == False):
         print("Error opening the video file")
     # Obtain frame size information using get() method
@@ -27,8 +21,8 @@ def process_video(input,output):
 
 
     # Initialize video writer object
-    output = cv2.VideoWriter(output, cv2.VideoWriter_fourcc(*'mp4v'), fps, frame_size)
-    with mp_holistic.Holistic(min_detection_confidence=0.7, min_tracking_confidence=0.7) as holistic:
+    output = cv2.VideoWriter(args.output, cv2.VideoWriter_fourcc(*'mp4v'), fps, frame_size)
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         while (cap.isOpened()):
     # vid_capture.read() methods returns a tuple, first element is a bool 
     # and the second is frame
@@ -51,6 +45,7 @@ def process_video(input,output):
                     landmarks = results.pose_landmarks.landmark
                     mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_holistic.POSE_CONNECTIONS,
                 landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+    # Draw left hand connections
                     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
                              mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
                              mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
@@ -73,11 +68,17 @@ def process_video(input,output):
         cap.release()
         output.release()
         cv2.destroyAllWindows()
+        
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Mediapipe video processing')
+    parser.add_argument('-i', "--input", dest='input', help='full path to input video that will be processed')
+    parser.add_argument('-o', "--output", dest='output', help='full path for saving processed video output')
+    args = parser.parse_args()
+
+    if args.input is None or args.output is None:
+        sys.exit("Please provide path to input and output video files! See --help")
 
 
-input_video = ("test.mp4")
-output_video = ("output.mp4")
-
-process_video(input_video,output_video)
-
-
+    process_video(args)
